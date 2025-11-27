fae4cd9de6690f1da2f2499894c1a814cf9d738a
ShawnYS-codemtl <syatsin@gmail.com>
2025-11-27 00:52:28 -0500

    feat: add ingredients to ingredients state using form

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,34 @@
 import React from "react"
 
 export default function Main(){
+
+    const [ingredients, setIngredients] = React.useState([])
+ 
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients((prevIngredients) => [...prevIngredients, newIngredient])
+    }
+
     return (
     <main>
-        <form className="add-ingredient-form">
-            <input type="text" aria-label="Add ingredient" placeholder="e.g. oregano"/>
+        <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <input 
+                type="text" 
+                aria-label="Add ingredient" 
+                placeholder="e.g. oregano"
+                name="ingredient"
+            />
             <button>Add ingredient</button>
         </form>
+        <ul>
+            {ingredientsListItems}
+        </ul>
     </main>
     )
 }
