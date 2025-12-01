2ba93afbd8c4fed691d0fa6460d87f1db016d255
ShawnYS-codemtl <syatsin@gmail.com>
2025-11-30 22:11:45 -0500

    refactor: create ClaudeRecipe and IngredientsList components

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,16 @@
 import React from "react"
+import ClaudeRecipe from "./ClaudeRecipe"
+import IngredientsList from "./IngredientsList"
 
 export default function Main(){
 
-    const [ingredients, setIngredients] = React.useState([])
- 
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
+    const [ingredients, setIngredients] = React.useState(["all main spices", "ground beef", "pasta", "tomato paste"])
+
+    const [recipeShown, setRecipeShown] = React.useState(false)
+
+    function getRecipe(){
+        setRecipeShown(true)
+    }
 
     function addIngredient(formData){
         const newIngredient = formData.get("ingredient")
@@ -24,18 +28,8 @@ export default function Main(){
             />
             <button>Add ingredient</button>
         </form>
-        {ingredients.length > 0 &&
-         <section>
-                <h2>Ingredients on hand:</h2>
-                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                {ingredients.length > 3 && <div className="get-recipe-container">
-                    <div>
-                        <h3>Ready for a recipe?</h3>
-                        <p>Generate a recipe from your list of ingredients.</p>
-                    </div>
-                    <button>Get a recipe</button>
-                </div>}
-            </section>}
+        {ingredients.length > 0 && <IngredientsList ingredients={ingredients} getRecipe={getRecipe}/>}
+        {recipeShown && <ClaudeRecipe/>}
     </main>
     )
 }
